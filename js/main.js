ee36b9ff03c1e8194aa1f9b96f18a60a05a64c6a
liuyuchen796 <181484908@qq.com>
2017-05-08 23:56:36 +0800

    ver 1.0

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var sswiperBanner = new Swiper('#banner>.bigphoto>.swiper-container');
+var swiper = new Swiper('#showList>.show>.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 3,
+        slidesPerColumn: 2,
+        paginationClickable: true,
+        spaceBetween: 30
+    });
