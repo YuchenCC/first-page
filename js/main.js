46ca39e70e27eca68beff9bc0b267e4e7b627fb6
liuyuchen <181484908@qq.com>
2017-05-09 16:06:00 +0800

    add响应式

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
 var sswiperBanner = new Swiper('#banner>.bigphoto>.swiper-container');
-var swiper = new Swiper('#showList>.show>.swiper-container', {
-        pagination: '.swiper-pagination',
-        slidesPerView: 3,
-        slidesPerColumn: 2,
-        paginationClickable: true,
-        spaceBetween: 30
-    });
+window.onload = function(){
+    let h = document.documentElement.clientWidth;
+    console.log(h)
+    if (h < 1030 && h > 797){
+        var swiper = new Swiper('#showList>.show>.swiper-container', {
+            pagination: '.swiper-pagination',
+            slidesPerView: 2,
+            slidesPerColumn: 2,
+            paginationClickable: true,
+            spaceBetween: 30
+        })
+    }
+    else if (h <= 797){
+            var swiper = new Swiper('#showList>.show>.swiper-container', {
+                pagination: '.swiper-pagination',
+                slidesPerView: 1,
+                slidesPerColumn: 3,
+                paginationClickable: true,
+                spaceBetween: 30
+            })
+        }
+        else {
+            var swiper = new Swiper('#showList>.show>.swiper-container', {
+                pagination: '.swiper-pagination',
+                slidesPerView: 3,
+                slidesPerColumn: 2,
+                paginationClickable: true,
+                spaceBetween: 30
+            })
+        }
+}
+
